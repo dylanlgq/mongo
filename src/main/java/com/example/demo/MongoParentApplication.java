af4273143d72382601f535363efd214ff5d08cb8
dylanlgq <iyuhzevol@163.com>
2018-04-11 01:13:12 +0800

    test

--- src/main/java/com/example/demo/MongoParentApplication.java
+++ src/main/java/com/example/demo/MongoParentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MongoParentApplication {
 
 	public static void main(String[] args) {
+		//ceshi
 		SpringApplication.run(MongoParentApplication.class, args);
 	}
 }
