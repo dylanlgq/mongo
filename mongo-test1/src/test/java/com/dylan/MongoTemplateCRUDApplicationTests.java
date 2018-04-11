61c7ddebf45a54f0c1e5a6d9b78aaf00e2a38ea8
dylanlgq <iyuhzevol@163.com>
2018-04-12 02:02:32 +0800

    mongo插入数据

--- mongo-test1/src/test/java/com/dylan/MongoTemplateCRUDApplicationTests.java
+++ mongo-test1/src/test/java/com/dylan/MongoTemplateCRUDApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.dylan;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Test1ApplicationTests {
+public class MongoTemplateCRUDApplicationTests {
 
 	@Test
 	public void contextLoads() {
