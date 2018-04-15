4e2691a382c749273ae2a9df2abc906f1e9e362a
dylanlgq <iyuhzevol@163.com>
2018-04-15 19:21:37 +0800

    。。

---
+++ mongo-template-CRUD/src/main/java/com/dylan/mongo/MongoTemplateCRUDApplication.java
@@ -0,0 +1,12 @@
+package com.dylan.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoTemplateCRUDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoTemplateCRUDApplication.class, args);
+	}
+}
